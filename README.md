cb8476e9503192dd3c9def7bcb1a1d0f64b8ac5d
Ion <iongireada@gmail.com>
2021-09-25 16:49:50 +0300

    Updated changrs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # skelo
 Generate files for Docusaurus-based documentation from outline files (.yaml or .md)
+
+## Description
+
+## Installation
+
+## Usage
+
+## Contributing
+
---
+++ index.js
@@ -0,0 +1,7 @@
+const program = require('commander')
+
+const version = require('./package.json').version;
+program
+    .description('Generate files for Docusaurus-powered documentation from outline files')
+    .version(versio
+
