c926cbdce1dfdcf7bd75d271d4765c13269b29ae
ioncakephper <shytiger@yahoo.com>
2021-09-27 16:29:18 +0300

    Changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const colors    = require("colors");
 const program   = require("commander");
 const fileEasy  = require("file-easy");
 const fs        = require("fs");
-const hbsr = require("hbsr");
-const path = require("path");
-
-
+const hbsr      = require("hbsr");
+const path      = require("path");
+const notifier  = require("./lib/notifier");
 const sklib     = require('./lib/sklib')
 const sktopic   = require('./lib/sktopic')
+
 
 let config;
 if (fs.existsSync('./skelo.json')) {
@@ -52,35 +53,39 @@ program
     "default extension for outline files",
     ".md"
   )
-  .option("-s, --sidebars <file>", "path to sidebars file", "./sidebars.js")
+  .option('-f, --folders', 'create folder for every parent topic')
+  .option("-s, --sidebars <file>", "path to sidebars file", "sidebars.js")
   .option("-d, --docs <path>", "path to docs folder", "./docs")
   .option("--summary", "generate summary pages for each parent topic")
   .option(
-    "--overview <string>",
+    "-o, --overview <string>",
     "Template string to use for summary pages",
     "{{label}} overview"
   )
   .option('-t, --templates <path>', 'path to templates folder', path.join(__dirname, 'templates'))
-  .option("--hide-summary", "hide summary pages for all summary pages unless")
+  // .option("--hide-summary", "hide summary pages for all summary pages unless")
+  .option('--no-verbose', 'inhibit sending progress messages to console. ')
 
   .action((outlines, options, commander) => {
       // console.log(JSON.stringify(options, null, 4))
-      console.log(JSON.stringify(commander.opts(), null, 4));
+      // console.log(JSON.stringify(commander.opts(), null, 4));
+    notifier.allow = options.verbose;
     outlines = outlines.map(pattern => fileEasy.setDefaultExtension(pattern, options.extension))
     outlines = outlines.filter(pattern => fs.existsSync(pattern))
 
     // console.log(JSON.stringify(outlines, null, 4));
 
     let allSidebars = sklib.buildAllSidebars(outlines, options, commander)
-    console.log(JSON.stringify(allSidebars, null, 4))
+    console.log('*** allSidebars:', JSON.stringify(allSidebars, null, 4))
     let allSidebarsLayout = sktopic.buildAllLayout(allSidebars, options, commander)
-    console.log(JSON.stringify(allSidebarsLayout, null, 4))
+    // console.log(JSON.stringify(allSidebarsLayout, null, 4))
 
     let sidebarsFilename = fileEasy.setDefaultExtension(options.sidebars, ".js")
     let content = hbsr.render_template('sidebars', {
         content: JSON.stringify(allSidebarsLayout, null, 4)
     })
     fileEasy.saveDocument(sidebarsFilename, content)
+    notifier.notify(`${sidebarsFilename} ${colors.green('generated')}`)
   });
 
 program
@@ -96,7 +101,11 @@ program
     "-e, --exclude <pattern...>",
     "folder or file patterns to exclude from removing from docs"
   )
-  .option("-c, --config <configuration>", "path to configuration file");
+  .option("-c, --config <configuration>", "path to configuration file")
+  .action((pattern, options, commander) => {
+    console.log('Not implemented yet')
+  })
 
 
-program.parse("node index.js init".split(/ +/g));
+// program.parse("node index.js build".split(/ +/g));
+program.parse()
--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,18 @@
-/**
- */
 module.exports = {
     "docs": [
-        "introduction"
+        "intro",
+        {
+            "type": "category",
+            "label": "Getting started",
+            "items": [
+                {
+                    "type": "ref",
+                    "id": "getting-started/getting-started-overview"
+                },
+                "getting-started/installation",
+                "getting-started/usage",
+                "getting-started/quick-demo"
+            ]
+        }
     ]
 }
